130a8bbb78e7cb8cf2e10fb42f8101e9b8ffc61f
Folgui <aguante.los.rulos@gmail.com>
2017-03-16 11:57:35 -0300

    primer TP

---
+++ Folgui.TPs/Clases/Alumnos.py
@@ -0,0 +1,13 @@
+class alumnos(object)
+    nombre = " "
+    apellido = " "
+    FechaNaciemiento = None
+    ListaNotas = []
+    def setNombre(self, Nombre):
+        self.nombre = str (Nombre)
+
+    def setApellido(self, Apellido):
+        self.apellido = str (Apellido)
+
+    def setFechaN(self, Fecha):
+        self.FechaNaciemiento = str (Fecha)
