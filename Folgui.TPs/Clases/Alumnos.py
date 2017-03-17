0b9e4c920b58fa0effaf0802151879331fd9b5cc
Folgui <aguante.los.rulos@gmail.com>
2017-03-17 11:33:40 -0300

    Ejercicio 2

--- Folgui.TPs/Clases/Alumnos.py
+++ Folgui.TPs/Clases/Alumnos.py
@@ -1,26 +1,63 @@
+from .Materia import materias
+
 class alumno(object):
+
     nombre = " "
     apellido = " "
     FechaNaciemiento = None
-    ListaNotas = []
-    def setNombre(self, Nombre):
-        self.nombre = str (Nombre)
+    ListaNotasA = []
+    ListaDeMaterias = []
 
-    def setApellido(self, Apellido):
-        self.apellido = str (Apellido)
+    def ___init___(self):
+        self.ListaDeMaterias = []
+
+
+    def Nombre(self, Nombre):
+        self.nombre = str(Nombre)
+
+    def Apellido(self, Apellido):
+        self.apellido = str(Apellido)
 
     def setFechaN(self, fecha):
         self.FechaNaciemiento = fecha
 
-    def agregarNota(self, Nota):
-        self.ListaNotas.append(Nota)
+    def setMateria(self, Mate):
+        nuevaMateria = materias()
+        nuevaMateria.NombreM(Mate)
+        self.ListaDeMaterias.append(Mate)
 
-    def menorNota(self):
-        return min(self.ListaNotas)
+    def encontrarMateria(self, Mat):
+        for item in self.ListaDeMaterias:
+            if item.nombre == Mat:
+                return item
 
-    def mayorNota(self):
-        return max(self.ListaNotas)
+    def agregarNotasM(self, mat, i):
+        self.encontrarMateria(mat).ListaNotasA(i)
 
-    def promedioNota(self):
-        return sum(self.ListaNotas) / len(self.ListaNotas)
+    def promMateria(self, n):
+        return float(sum(self.encontrarMateria(n).ListaNotasA) / len(self.encontrarMateria(n).ListaNotasA))
+
+    def promAlumno(self):
+        a = 0
+        for item in self.ListaDeMaterias:
+            a += item.promMateria(item.nombre)
+            return float(a/self.ListaDeMaterias)
+
+    def menorYmayorProm(self):
+        a = []
+        for item in self.ListaDeMaterias:
+            a.append(item.promMateria(item.nombre))
+            return min(a), max(a)
+
+    def agregarNota(self, h):
+        self.ListaNotasA.append(h)
+
+    def menor_nota(self):
+        return min(self.ListaNotasA)
+
+    def mayor_nota(self):
+        return max(self.ListaNotasA)
+
+    def prom(self):
+        return sum(self.ListaNotasA) / len(self.ListaNotasA)
 
---
+++ Folgui.TPs/Clases/Materia.py
@@ -0,0 +1,11 @@
+class materias(object):
+
+    NombreM = " "
+    ListaNotasM = []
+
+
+    def setNombre(self, Mate):
+        self.nombre = Mate
+
+    def ___init___(self):
+        self.ListaNotasM = []
